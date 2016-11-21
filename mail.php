24b9270427f8660a79c4f4633b79a09e7e97c3f0
Yurii321 <uliana2nikorovi4@yandex.ua>
2016-11-21 20:02:42 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,10 +1,29 @@
 <?php
   if(isset($_POST["send"])) {
+    $to = "yura05158@gmail.com";
     $name = htmlspecialchars ($_POST["name"]);
     $email = htmlspecialchars ($_POST["email"]);
     $message = htmlspecialchars ($_POST["message"]);
-    $_SESSION["name"] = $name;
-    $_SESSION["email"] = $email;
-    $_SESSION["message"] = $message;
+    $error_name = "";
+    $error_email = "";
+    $error_message = "";
+    $error = false;
+    if($email == "" || !preg_match ("/@/", $email)) {
+    $error_email = "ERROR";
+    $error = true;
+    }
+    if(strlen($name) == 0) {
+     $error_name = "ERROR";
+     $error = true;
+    }
+    if(strlen($message) == 0) {
+     $error_message = "ERROR";
+     $error = true;
+    }
+    if(!error) {
+      $subject = "Tema";
+      $headers = "From: $email\r\nReply-to: $email\r\nContent-type: text/html; charset=utf-8\r\n";
+      mail ($to, $subject, $name, $message, $headers);
+    }
   }
 ?>
